43ec0ab76df9d1cd6bee02814f1c500757ebe8fb
Cory Mathis <coryrmathis@gmail.com>
2017-06-20 22:35:56 -0700

    Seeds written

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,13 @@
 class Cocktail < ApplicationRecord
+  # Associations
   has_and_belongs_to_many :ingredients
+
+  # Validations
+  validates :name, uniqueness: true, presence: true
+
+
+  def self.create_with_ingredients(args)
+    ingredients = args[:ingredients].map {|i| Ingredient.find_or_create_by(name: i)}
+    Cocktail.create(name: args[:name], ingredients: ingredients)
+  end
 end
